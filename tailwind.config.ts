df3c1e02f34df7584da3716cd3e7f980f3ea76cf
Jack Michell <jmichell2003@gmail.com>
2023-09-20 15:13:44 -0500

    added colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: "#05AFCD",
+      secondary: "#40FF80",
+      offset: "#004040",
+      dark: "#1F1F1F",
+      darker: "#000000",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
