aa107344af3126b19096638734feb27be18308fc
Jack Michell <jmichell2003@gmail.com>
2023-09-24 10:12:21 -0500

    Added some small styling for the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,8 +3,8 @@ import FooterImg from "../../public/images/footer.png";
 
 export default function Footer() {
   return (
-    <div>
-      <footer>
+    <div className="bg-dark p-4">
+      <footer className="flex justify-center items-center">
         <Image src={FooterImg} alt="Footer Image" width={300} />
       </footer>
     </div>
