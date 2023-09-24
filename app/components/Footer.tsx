575b34d4cb50689c464fc1d7856e2619b1dba120
Jack Michell <jmichell2003@gmail.com>
2023-09-24 10:08:10 -0500

    Added footer image

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import FooterImg from "../../public/images/footer.png";
+
 export default function Footer() {
-  return <div>&copy; Helvera 2023</div>;
+  return (
+    <div>
+      <footer>
+        <Image src={FooterImg} alt="Footer Image" width={300} />
+      </footer>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,13 @@ export default function Home() {
         <Image src={Hero} alt="Hero Image" className="w-full" />
       </section>
       {/* Services */}
-      <section>Services</section>
+      <section>Here are some of the services we provide.</section>
       {/* Advantages */}
-      <section>Advantages</section>
+      <section>Here are the advantages of our services.</section>
       {/* Features */}
-      <section>{/* Use Grid for Features */}Features</section>
+      <section>
+        {/* Use Grid for Features */}Here are the features of our services
+      </section>
       {/* Partners and Vendors */}
       <section>Partners and Vendors</section>
     </div>
