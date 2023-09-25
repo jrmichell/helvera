696b5165220847067449daabb4f0a77a64f11f67
Jack Michell <jmichell2003@gmail.com>
2023-09-24 20:31:57 -0500

    fix: footer
    The footer now sticks to the bottom of every page, and it is also responsive

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,9 +3,13 @@ import FooterImg from "../../public/images/footer.png";
 
 export default function Footer() {
   return (
-    <div className="bg-dark p-4">
+    <div className="bg-dark p-4 mt-auto">
       <footer className="flex justify-center items-center">
-        <Image src={FooterImg} alt="Footer Image" width={300} />
+        <Image
+          src={FooterImg}
+          alt="Footer Image"
+          className="w-[150px] sm:w-[225px] md:w-[300px]"
+        />
       </footer>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={`${poppins.className} flex flex-col min-h-screen`}>
         <Navbar />
         {children}
         <Footer />
