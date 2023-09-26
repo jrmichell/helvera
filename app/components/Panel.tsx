957813420069f7d7a907a1c970b5510b4d4fae8a
Alex George <alex@basis.yt>
2023-09-27 00:47:02 +0100

    fix: updated width of panels on homepage

--- app/components/Panel.tsx
+++ app/components/Panel.tsx
@@ -8,12 +8,12 @@ interface PanelProps {
 // Define the Panel component
 const Panel: React.FC<PanelProps> = ({ children, title, content }) => {
   return (
-    <div className="p-8 flex justify-center items-center flex-col max-w-[500px]">
+    <div className="p-8 flex justify-center items-center flex-col max-w-[400px]">
       <div className="flex justify-center items-center mb-2 text-primary">
         {children}
       </div>
       <h1 className="text-xl font-bold mb-2">{title}</h1>
-      <p className="sm:text-lg md:text-md text-sm text-center">{content}</p>
+      <p className="sm:text-md text-sm text-center">{content}</p>
     </div>
   );
 };
