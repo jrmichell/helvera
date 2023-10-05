50301d65584d08b7ac103f67fe3d65314032f414
Jack Michell <123218979+JackMichell@users.noreply.github.com>
2023-10-05 04:27:12 -0500

    Fixed Coming Soon card being positioned incorrectly

--- app/vps/page.tsx
+++ app/vps/page.tsx
@@ -2,7 +2,7 @@ import Card from "../components/Card";
 
 export default function VPS() {
   return (
-    <div className="absolute top-[25%] left-[45%]">
+    <div>
       {/* Coming Soon */}
       <Card title="Coming Soon" content="This page is unavailable" />
     </div>
